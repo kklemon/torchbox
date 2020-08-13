fc1019b92df35a075a703715c308aee45e255916
Kristian Klemon <klemon@unternehmertum.de>
2020-08-13 09:57:51 +0200

    Do not add timestamp to run name by default in setup_run

--- torchbox/utils/misc.py
+++ torchbox/utils/misc.py
@@ -13,10 +13,13 @@ def get_default_device(default: Optional[Union[str, torch.device]] = None):
     return device
 
 
-def setup_run(run_name, results_dir='results', create_dirs=('checkpoints', 'samples')):
+def setup_run(run_name=None, results_dir='results', create_dirs=('checkpoints', 'samples'), datefmt='%Y-%m-%d_%H-%M-%S'):
+    if not run_name:
+        run_name = datetime.now().strftime(datefmt)
+
     results_dir = Path(results_dir)
 
-    run_dir = results_dir / (datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + f'-{run_name}')
+    run_dir = results_dir / run_name
     run_dir.mkdir(parents=True)
 
     for d in create_dirs:
