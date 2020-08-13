d6eb0d510a57383d89853baa858f10fe6e12e4ca
Kristian Klemon <klemon@unternehmertum.de>
2020-08-13 09:58:56 +0200

    Provide more GAN losses

--- torchbox/losses/functional.py
+++ torchbox/losses/functional.py
@@ -3,13 +3,13 @@ import torch.nn.functional as F
 
 
 def standard_gan_loss_d(D, reals, fakes):
-    reals_score = D(reals)
-    reals_loss = F.binary_cross_entropy_with_logits(reals_score, torch.ones_like(reals_score))
+    real_scores = D(reals)
+    real_loss = F.binary_cross_entropy_with_logits(real_scores, torch.ones_like(real_scores))
 
-    fakes_score = D(fakes)
-    fakes_loss = F.binary_cross_entropy_with_logits(fakes_score, torch.zeros_like(fakes_score))
+    fake_scores = D(fakes)
+    fake_loss = F.binary_cross_entropy_with_logits(fake_scores, torch.zeros_like(fake_scores))
 
-    loss = (reals_loss + fakes_loss) / 2.0
+    loss = (real_loss + fake_loss) / 2.0
 
     return loss
 
@@ -19,3 +19,69 @@ def standard_gan_loss_g(D, fakes):
     fakes_loss = F.binary_cross_entropy_with_logits(fakes_score, torch.ones_like(fakes_score))
 
     return fakes_loss
+
+
+def gradient_penalty(D, reals, fakes, reg_lambda=10):
+    is_sample_list = True
+    if not isinstance(reals, (list, tuple)):
+        assert not isinstance(fakes, (list, tuple))
+        reals = [reals]
+        fakes = [fakes]
+        is_sample_list = False
+
+    reals = [real.detach() for real in reals]
+    fakes = [fake.detach() for fake in fakes]
+
+    batch_size = reals[0].size(0)
+    num_dims = reals[0].ndim - 1
+
+    # Draw random epsilon
+    eps = torch.rand((batch_size, *[1] * num_dims)).to(reals[0].device)
+
+    # Create the merge of both real and fake samples
+    merged = [eps * real + (1 - eps) * fake for real, fake in zip(reals, fakes)]
+    for sample in merged:
+        sample.requires_grad = True
+
+    op = D(merged if is_sample_list else merged[0])
+
+    # Perform backward pass from op to merged for obtaining the gradients
+    gradients = torch.autograd.grad(outputs=op, inputs=merged,
+                                    grad_outputs=torch.ones_like(op), create_graph=True,
+                                    retain_graph=True, only_inputs=True)
+
+    # Calculate the penalty using these gradients
+    penalties = [(gradient.reshape(gradient.size(0), -1).norm(p=2, dim=1) ** 2).mean().unsqueeze(0)
+                 for gradient in gradients]
+    penalty = reg_lambda * torch.cat(penalties).mean()
+
+    return penalty
+
+
+def wasserstein_gan_loss_d(D, reals, fakes, drift=0.001):
+    fake_scores = D(fakes)
+    real_scores = D(reals)
+
+    loss = fake_scores.mean() - real_scores.mean() + drift * (real_scores ** 2).mean()
+
+    return loss
+
+
+def wasserstein_gan_loss_g(D, fakes):
+    return -D(fakes).mean()
+
+
+def relativistic_average_gan_loss_d(D, reals, fakes, f1, f2):
+    real_out = D(reals)
+    fake_out = D(fakes)
+
+    rf_diff = real_out - fake_out.mean()
+    fr_diff = fake_out - real_out.mean()
+
+    loss = f1(rf_diff).mean() + f2(fr_diff).mean()
+
+    return loss
+
+
+def relativistic_average_gan_loss_g(D, reals, fakes, g1, g2):
+    return relativistic_average_gan_loss_d(D, fakes, reals, g1, g2)
---
+++ torchbox/losses/gan.py
@@ -0,0 +1,74 @@
+import torch.nn.functional as F
+
+from torchbox.losses.functional import (
+    standard_gan_loss_d,
+    standard_gan_loss_g,
+    gradient_penalty,
+    wasserstein_gan_loss_d,
+    wasserstein_gan_loss_g,
+    relativistic_average_gan_loss_d,
+    relativistic_average_gan_loss_g
+)
+
+
+class BaseGANLossFunction:
+    def loss_d(self, reals, fakes):
+        raise NotImplementedError
+
+    def loss_g(self, reals, fakes):
+        raise NotImplementedError
+
+
+class StandardGANLoss(BaseGANLossFunction):
+    def __init__(self, D):
+        self.D = D
+
+    def loss_d(self, reals, fakes):
+        return standard_gan_loss_d(self.D, reals, fakes)
+
+    def loss_g(self, reals, fakes):
+        return standard_gan_loss_g(self.D, fakes)
+
+
+class WassersteinGANLoss(BaseGANLossFunction):
+    def __init__(self, D, use_gp=True, reg_lambda=10, drift=0.001):
+        self.D = D
+        self.use_gp = use_gp
+        self.reg_lambda = reg_lambda
+        self.drift = drift
+
+    def gradient_penalty(self, reals, fakes):
+        return gradient_penalty(self.D, reals, fakes, reg_lambda=self.reg_lambda)
+
+    def loss_d(self, reals, fakes):
+        loss = wasserstein_gan_loss_d(self.D, reals, fakes, drift=self.drift)
+
+        if self.use_gp and self.reg_lambda:
+            loss += self.gradient_penalty(reals, fakes)
+
+        return loss
+
+    def loss_g(self, reals, fakes):
+        return wasserstein_gan_loss_g(self.D, fakes)
+
+
+class RelativisticAverageGANLoss(BaseGANLossFunction):
+    def __init__(self, D, f1, f2, g1, g2):
+        self.D = D
+        self.f1 = f1
+        self.f2 = f2
+        self.g1 = g1
+        self.g2 = g2
+
+    def loss_d(self, reals, fakes):
+        return relativistic_average_gan_loss_d(self.D, reals, fakes, self.f1, self.f2)
+
+    def loss_g(self, reals, fakes):
+        return relativistic_average_gan_loss_g(self.D, reals, fakes, self.g1, self.g2)
+
+
+class RelativisticAverageHingeGANLoss(RelativisticAverageGANLoss):
+    def __init__(self, D):
+        f1 = lambda x: F.relu(1 - x)
+        f2 = lambda x: F.relu(1 + x)
+        super().__init__(D, f1, f2, f1, f2)
